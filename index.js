a84a44e014d8a2bd60a71cd2a9dd8b94bc5960ac
Engin Karataş <enginkaratas99@gmail.com>
2021-03-21 20:36:38 +0300

    express index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+
+//server codes index.js
+const fs = require('fs') //filestream
+const express = require('express');
+const app = express(); //listining right now
+app.listen(3000, () => console.log("listening at 3000")); //3000 de dinleyeceğim
+app.use(express.static('public'));//kullanıcı tarafından erişilebilecek dosya 'public'dir.
+app.use(express.json({limit:"1mb"}));//server allows json and taken data size max 1mb, If this row not exist it will be undifined for request parameter 
+//---2.3--- post data and recive data 
+
 
 app.get('/salamon', (req, res) => {
   res.send('Hello World!')
